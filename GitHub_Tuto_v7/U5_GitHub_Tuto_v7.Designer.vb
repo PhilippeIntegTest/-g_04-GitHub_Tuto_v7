d7686352b29798d0db62367270b538912f516af7
PhilippeIntegTest <Philippe.chesnel_Gith1@gadzarts.org>
2024-05-02 20:57:29 +0200

    Branch_Add_Label_Purple  - Add Label purple !

--- GitHub_Tuto_v7/U5_GitHub_Tuto_v7.Designer.vb
+++ GitHub_Tuto_v7/U5_GitHub_Tuto_v7.Designer.vb
@@ -25,7 +25,7 @@ Partial Class U5_GitHub_Tuto_v7
 		Me.Label1 = New System.Windows.Forms.Label()
 		Me.Label2 = New System.Windows.Forms.Label()
 		Me.Label3 = New System.Windows.Forms.Label()
-		Me.Label4 = New System.Windows.Forms.Label()
+		Me.Label5 = New System.Windows.Forms.Label()
 		Me.SuspendLayout()
 		'
 		'Label1
@@ -70,19 +70,19 @@ Partial Class U5_GitHub_Tuto_v7
 		Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
 		Me.Label3.UseCompatibleTextRendering = True
 		'
-		'Label4
+		'Label5
 		'
-		Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
-		Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-		Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Label4.ForeColor = System.Drawing.Color.Maroon
-		Me.Label4.Location = New System.Drawing.Point(55, 108)
-		Me.Label4.Name = "Label4"
-		Me.Label4.Size = New System.Drawing.Size(412, 16)
-		Me.Label4.TabIndex = 4
-		Me.Label4.Text = "Git: Dans Branch Add_Label_Purple (Check-In 20h48)"
-		Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-		Me.Label4.UseCompatibleTextRendering = True
+		Me.Label5.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+		Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label5.ForeColor = System.Drawing.Color.Maroon
+		Me.Label5.Location = New System.Drawing.Point(55, 108)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(412, 16)
+		Me.Label5.TabIndex = 4
+		Me.Label5.Text = "Git: Dans Branch Add_Label_Purple (Check-In 20h48)"
+		Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+		Me.Label5.UseCompatibleTextRendering = True
 		'
 		'U5_GitHub_Tuto_v7
 		'
@@ -90,7 +90,7 @@ Partial Class U5_GitHub_Tuto_v7
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
 		Me.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-		Me.Controls.Add(Me.Label4)
+		Me.Controls.Add(Me.Label5)
 		Me.Controls.Add(Me.Label3)
 		Me.Controls.Add(Me.Label2)
 		Me.Controls.Add(Me.Label1)
@@ -103,5 +103,5 @@ Partial Class U5_GitHub_Tuto_v7
 	Friend WithEvents Label1 As Label
 	Friend WithEvents Label2 As Label
 	Friend WithEvents Label3 As Label
-	Friend WithEvents Label4 As Label
+	Friend WithEvents Label5 As Label
 End Class
