860ce886ad0ede68b354d08ad66663617a567ca8
PhilippeIntegTest <Philippe.chesnel_Gith1@gadzarts.org>
2024-05-02 20:48:53 +0200

    (Branch Add Label Purple)  Add Label purple

--- GitHub_Tuto_v7/U5_GitHub_Tuto_v7.Designer.vb
+++ GitHub_Tuto_v7/U5_GitHub_Tuto_v7.Designer.vb
@@ -25,6 +25,7 @@ Partial Class U5_GitHub_Tuto_v7
 		Me.Label1 = New System.Windows.Forms.Label()
 		Me.Label2 = New System.Windows.Forms.Label()
 		Me.Label3 = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
 		Me.SuspendLayout()
 		'
 		'Label1
@@ -69,17 +70,32 @@ Partial Class U5_GitHub_Tuto_v7
 		Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
 		Me.Label3.UseCompatibleTextRendering = True
 		'
+		'Label4
+		'
+		Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+		Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label4.ForeColor = System.Drawing.Color.Maroon
+		Me.Label4.Location = New System.Drawing.Point(55, 108)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(412, 16)
+		Me.Label4.TabIndex = 4
+		Me.Label4.Text = "Git: Dans Branch Add_Label_Purple (Check-In 20h48)"
+		Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+		Me.Label4.UseCompatibleTextRendering = True
+		'
 		'U5_GitHub_Tuto_v7
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
 		Me.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.Controls.Add(Me.Label4)
 		Me.Controls.Add(Me.Label3)
 		Me.Controls.Add(Me.Label2)
 		Me.Controls.Add(Me.Label1)
 		Me.Name = "U5_GitHub_Tuto_v7"
-		Me.Size = New System.Drawing.Size(502, 148)
+		Me.Size = New System.Drawing.Size(502, 205)
 		Me.ResumeLayout(False)
 
 	End Sub
@@ -87,4 +103,5 @@ Partial Class U5_GitHub_Tuto_v7
 	Friend WithEvents Label1 As Label
 	Friend WithEvents Label2 As Label
 	Friend WithEvents Label3 As Label
+	Friend WithEvents Label4 As Label
 End Class
