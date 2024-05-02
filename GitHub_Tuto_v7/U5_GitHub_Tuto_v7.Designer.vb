fdced129aa8fc8ae27e4f283b6c7c7fa89effaa4
PhilippeIntegTest <Philippe.chesnel_Gith1@gadzarts.org>
2024-05-02 20:38:00 +0200

    Add Label Yellow

--- GitHub_Tuto_v7/U5_GitHub_Tuto_v7.Designer.vb
+++ GitHub_Tuto_v7/U5_GitHub_Tuto_v7.Designer.vb
@@ -25,6 +25,7 @@ Partial Class U5_GitHub_Tuto_v7
 		Me.Label1 = New System.Windows.Forms.Label()
 		Me.Label2 = New System.Windows.Forms.Label()
 		Me.Label3 = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
 		Me.SuspendLayout()
 		'
 		'Label1
@@ -60,7 +61,7 @@ Partial Class U5_GitHub_Tuto_v7
 		Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
 		Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
 		Me.Label3.ForeColor = System.Drawing.Color.Maroon
-		Me.Label3.Location = New System.Drawing.Point(55, 59)
+		Me.Label3.Location = New System.Drawing.Point(55, 55)
 		Me.Label3.Name = "Label3"
 		Me.Label3.Size = New System.Drawing.Size(412, 16)
 		Me.Label3.TabIndex = 3
@@ -69,12 +70,27 @@ Partial Class U5_GitHub_Tuto_v7
 		Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
 		Me.Label3.UseCompatibleTextRendering = True
 		'
+		'Label4
+		'
+		Me.Label4.BackColor = System.Drawing.Color.Yellow
+		Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label4.ForeColor = System.Drawing.Color.Maroon
+		Me.Label4.Location = New System.Drawing.Point(55, 75)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(412, 16)
+		Me.Label4.TabIndex = 4
+		Me.Label4.Text = "Git: Branch Feature -->  Add Label Yellow"
+		Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+		Me.Label4.UseCompatibleTextRendering = True
+		'
 		'U5_GitHub_Tuto_v7
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
 		Me.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.Controls.Add(Me.Label4)
 		Me.Controls.Add(Me.Label3)
 		Me.Controls.Add(Me.Label2)
 		Me.Controls.Add(Me.Label1)
@@ -87,4 +103,5 @@ Partial Class U5_GitHub_Tuto_v7
 	Friend WithEvents Label1 As Label
 	Friend WithEvents Label2 As Label
 	Friend WithEvents Label3 As Label
+	Friend WithEvents Label4 As Label
 End Class
